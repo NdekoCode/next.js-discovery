69e35c9ada6842dbc545eb3cc5c068c3c2272314
NdekoCode <arickbulakali@gmail.com>
2023-08-16 00:37:07 +0200

    feat: init page folter routing

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+const contact = () => {
+  return (
+    <div>
+      <h1>Nous contacter</h1>
+    </div>
+  );
+};
+export default contact;
---
+++ app/cv/page.js
@@ -0,0 +1,8 @@
+const cv = () => {
+  return (
+    <div>
+      <h1>Mon CV</h1>
+    </div>
+  );
+};
+export default cv;
