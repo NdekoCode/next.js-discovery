9128a6740aa65ebe54a08431ec8d64a0bc605493
NdekoCode <arickbulakali@gmail.com>
2023-08-17 06:32:37 +0200

    feat: add custom hoom for fetching data API

--- app/context/theme.js
+++ app/context/theme.js
@@ -1,1 +1,48 @@
-// import {createContext, useContext,useMemo,useState,memo} from 'react';
+"use client";
+import {
+  createContext,
+  memo,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+const AppContext = createContext({});
+export const AppContextProvider = memo(({ children }) => {
+  const [background, setBackground] = useState("lightgreen");
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const responseData = await response.json();
+        if (response.ok) {
+          setUsers(responseData);
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+        }
+      } catch (error) {
+        setIsLoading(false);
+      }
+    })();
+  }, [isLoading]);
+  const value = useMemo(
+    () => ({
+      background,
+      setBackground,
+      users,
+      setUsers,
+      isLoading,
+      setIsLoading,
+    }),
+    [background, users, isLoading]
+  );
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+});
+const useAppContext = () => useContext(AppContext);
+export default useAppContext;
--- app/users/page.js
+++ app/users/page.js
@@ -1,39 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
 import UserItem from "../components/UserItem";
+import useAppContext from "../context/theme";
 const users = () => {
-  const [data, setData] = useState({
-    users: [],
-    isLoading: true,
-  });
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const responseData = await response.json();
-        if (response.ok) {
-          setData((state) => ({
-            ...state,
-            users: responseData,
-            isLoading: false,
-          }));
-        } else {
-          setData((state) => ({ ...state, isLoading: false }));
-        }
-      } catch (error) {
-        setData((state) => ({ ...state, isLoading: false }));
-      }
-    })();
-  }, [data.isLoading]);
+  const { users, isLoading } = useAppContext();
   return (
     <article>
       <div className="max-w-md p-4 mx-auto bg-white border rounded-lg shadow-md sm:p-8 ">
-        {data.isLoading && <p>Loading...</p>}
+        {isLoading && <p>Loading...</p>}
         <div className="flow-root">
+          {}
           <ul role="list" className="divide-y divide-gray-200 ">
-            {data.users.map((user) => (
+            {users?.map((user) => (
               <UserItem key={user.id} user={user} />
             ))}
           </ul>
