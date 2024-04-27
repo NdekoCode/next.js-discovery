0c97a0ecd952049750905b47dd0b4c8c9694b2e4
Ndekocode <arickbulakali@gmail.com>
2024-04-27 10:07:55 +0200

    feat: add section zod validation to login page

--- components/layouts/Container.tsx
+++ components/layouts/Container.tsx
@@ -1,14 +1,15 @@
-import { FunctionComponent, PropsWithChildren } from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-const Container: FunctionComponent<PropsWithChildren> = ({ children }) => {
+import { FunctionComponent, PropsWithChildren } from 'react';
+
+import Footer from './Footer';
+import Header from './Header';
+
+const AppContainer: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
     <section className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-grow">
-      {children}</div>
-      <Footer/>
+      <div className="flex-grow">{children}</div>
+      <Footer />
     </section>
   );
 };
-export default Container;
+export default AppContainer;
