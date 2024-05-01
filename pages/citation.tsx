df6022583f13f3b25c24301473518c0b09379a7a
ndekocode <arickbulakali@gmail.com>
2024-05-01 07:40:31 +0200

    feat: test ISR with quote generation

---
+++ pages/citation.tsx
@@ -0,0 +1,54 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+type Quote = {
+  quote: string;
+  author: string;
+  category: string;
+};
+
+const citation: FC<{ citations: Quote[] }> = (props) => {
+  console.log(props);
+  const citations = props.citations;
+  return (
+    <>
+      <Head>
+        <title>Citations</title>
+      </Head>
+      <div className="flex items-center justify-center mt-10">
+        {citations.map((citation, index) => (
+          <div key={index} className="flex flex-col items-center justify-center h-56 gap-5 p-5 text-white bg-blue-500 rounded-3xl w-96">
+            <p className="text-base text-centr">{citation.quote}</p>
+            <p className="text-base text-centr">{citation.author}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export const getStaticProps = async () => {
+  if (!process.env.NEXT_QUOTE_API_KEY) {
+    console.log("NEXT_QUOTE_API is NOT DEFINED");
+    return {
+      props: {
+        citations: [],
+      },
+    };
+  }
+  const citations = await (
+    await fetch("https://api.api-ninjas.com/v1/quotes?category=best", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": process.env.NEXT_QUOTE_API_KEY,
+      },
+    })
+  ).json();
+  return {
+    props: {
+      citations,
+    },
+    revalidate:20
+  };
+};
+export default citation;
--- pages/english.tsx
+++ pages/english.tsx
@@ -1,52 +1,58 @@
-import { FC } from 'react';
+import Head from "next/head";
+import { FC } from "react";
 
 type LangKey = "fr" | "en";
 const EnglishLearning: FC<{
   vocabularies: { [key in LangKey]: string }[];
 }> = ({ vocabularies }) => {
   return (
-    <div>
-      {vocabularies ? (
-        <div className="flex flex-col">
-          <div className="-m-1.5 overflow-x-auto">
-            <div className="p-1.5 min-w-full inline-block align-middle">
-              <div className="overflow-hidden">
-                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        English
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        French
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
-                    {vocabularies.map((vocabulary, index) => (
-                      <tr key={index}>
-                        <td className="px-6 py-4 text-sm font-medium text-gray-800 capitalize whitespace-nowrap dark:text-neutral-200">
-                          {vocabulary.en}
-                        </td>
-                        <td className="px-6 py-4 text-sm text-gray-800 capitalize whitespace-nowrap dark:text-neutral-200">
-                          {vocabulary.fr}
-                        </td>
+    <>
+      <Head>
+        <title>Learn English</title>
+      </Head>
+      <div>
+        {vocabularies ? (
+          <div className="flex flex-col">
+            <div className="-m-1.5 overflow-x-auto">
+              <div className="p-1.5 min-w-full inline-block align-middle">
+                <div className="overflow-hidden">
+                  <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                    <thead>
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                        >
+                          English
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                        >
+                          French
+                        </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+                      {vocabularies.map((vocabulary, index) => (
+                        <tr key={index}>
+                          <td className="px-6 py-4 text-sm font-medium text-gray-800 capitalize whitespace-nowrap dark:text-neutral-200">
+                            {vocabulary.en}
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-800 capitalize whitespace-nowrap dark:text-neutral-200">
+                            {vocabulary.fr}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ) : null}
-    </div>
+        ) : null}
+      </div>
+    </>
   );
 };
 export const getStaticProps = async () => {
