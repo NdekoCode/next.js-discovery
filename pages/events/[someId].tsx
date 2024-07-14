c811dc200518430e73083e64b08c6f432e320da0
Ndekocode <arickbulakali@gmail.com>
2024-07-15 00:48:05 +0200

    add get static props to events

--- pages/events/[someId].tsx
+++ pages/events/[someId].tsx
@@ -1,10 +1,13 @@
-import { NextPage } from 'next';
+import { readFile } from 'fs/promises';
+import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { join } from 'path';
 
 import DataNotFound from '../../components/DataNotFound';
 import { EventDetails } from '../../components/events';
 import { getEventById } from '../../utils/data/constants';
+import { Event } from '../../utils/types';
 
 type SingleEventProps = {
   someId: string;
@@ -16,13 +19,47 @@ const SingleEvent: NextPage<SingleEventProps> = () => {
   return (
     <>
       <Head>
-              <title>My Event {someId}</title>
+        <title>My Event {someId}</title>
       </Head>
       <div className="container my-20">
-        {event ? <EventDetails event={event}/>: <DataNotFound/>}
+        {event ? <EventDetails event={event} /> : <DataNotFound />}
       </div>
     </>
   );
+};
+export const getStaticPaths = async () => {
+  const path = join(process.cwd(), "utils", "data", "events.json");
+  const { events } = JSON.parse(
+    await readFile(path, {
+      encoding: "utf-8",
+    })
+  ) as { events: Event[] };
+  const paths = events.map((item) => ({
+    params: {
+      someId: item.id,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps<{ event: Event }> = async (
+  context
+) => {
+  const { params } = context;
+  const path = join(process.cwd(), "utils", "data", "events.json");
+  const { events } = JSON.parse(
+    await readFile(path, {
+      encoding: "utf-8",
+    })
+  ) as { events: Event[] };
+  const event = events.find((event) => event.id === params?.someId)!;
+  return {
+    props: {
+      event,
+    },
+  };
 };
 
 export default SingleEvent;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,30 +1,43 @@
-import { NextPage } from 'next';
+import { readFile } from 'fs/promises';
+import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { join } from 'path';
 
 import { EventSearch, EventsList } from '../../components/events';
-import { DUMMY_EVENTS } from '../../utils/data/constants';
+import { Event } from '../../utils/types';
 
-const EventPage: NextPage = () => {
-    const router = useRouter();
-    const [ events, setEvents ] = useState(DUMMY_EVENTS);
-    const findEventHandler = (selectedYear: string, selectedMonth: string) => {
-        if (selectedYear !== '' && selectedMonth !== '') {
-            const fullPath = `/events/${selectedYear}/${selectedMonth}`;
-            router.push(fullPath)
-         }
-        
+const EventPage: NextPage<{ events: Event[] }> = ({ events }) => {
+  const router = useRouter();
+  const findEventHandler = (selectedYear: string, selectedMonth: string) => {
+    if (selectedYear !== "" && selectedMonth !== "") {
+      const fullPath = `/events/${selectedYear}/${selectedMonth}`;
+      router.push(fullPath);
     }
-    return <>
-        <Head>
-            <title>Our events</title>
-        </Head>
-        <div className="container mt-10">
-            <EventSearch onSearch={findEventHandler} />
-            <EventsList events={events} />
-        </div>
+  };
+  return (
+    <>
+      <Head>
+        <title>Our events</title>
+      </Head>
+      <div className="container mt-10">
+        <EventSearch onSearch={findEventHandler} />
+        <EventsList events={events} />
+      </div>
     </>
-}
-
+  );
+};
+export const getStaticProps: GetStaticProps<{ events: Event[] }> = async (
+  context
+) => {
+  const path = join(process.cwd(), "utils", "data", "events.json");
+  const data = await readFile(path, {
+    encoding: "utf-8",
+  });
+  const { events } = JSON.parse(data) as { events: Event[] };
+  console.log(events);
+  return {
+    props: { events },
+  };
+};
 export default EventPage;
