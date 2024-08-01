2b8561a31e20aecff8279ae268f9e8140ce6b3e3
Ndekocode <arickbulakali@gmail.com>
2024-08-01 12:14:05 +0200

    retrieve single event

--- utils/helpers/api.ts
+++ utils/helpers/api.ts
@@ -1,12 +1,11 @@
-
 import { Event } from '../types/index';
 
 export const fetcher = (...args: Parameters<typeof fetch>) =>
   fetch(...args).then((res) => res.json());
 export const getAllEvents = async () => {
-  const events = await (await fetch(
-    "http://localhost:3000/api/events"
-  )).json() as Event[];
+  const events = (await (
+    await fetch("http://localhost:3000/api/events")
+  ).json()) as Event[];
   if (!events) {
     return null;
   }
@@ -14,11 +13,12 @@ export const getAllEvents = async () => {
 };
 
 export const getSingleEvent = async (eventId: string) => {
-  const events = await getAllEvents();
-  if (!events) {
+  const event = (await (
+    await fetch(`http://localhost:3000/api/events/${eventId}`)
+  ).json()) as Event;
+  if (!event) {
     return null;
   }
-  const event = events?.find((event) => event.id === eventId)!;
   return event;
 };
 export const getAllFilteredEvents = async (dateFilter: {
